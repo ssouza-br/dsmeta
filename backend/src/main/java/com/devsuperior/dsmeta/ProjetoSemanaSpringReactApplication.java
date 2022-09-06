c093438bc3fbd7761a59eec9d01b8cb90d061392
Sergio Souza <sergio_unicamp@yahoo.com.br>
2022-09-05 22:30:12 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsmeta/ProjetoSemanaSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSemanaSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSemanaSpringReactApplication.class, args);
+	}
+
+}
