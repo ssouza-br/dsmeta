74f08119bb964226ca010a52213abf6dcd4de1b6
Sergio Souza <sergio_unicamp@yahoo.com.br>
2022-09-07 19:33:10 -0300

    Primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./components/Notification button"
+
 function App() {
   return(
-    <h1>"Olá mundo"</h1>
+    <>
+      <h1>"Olá mundo"</h1>
+      <NotificationButton/>
+    </>
   )
 }
 
