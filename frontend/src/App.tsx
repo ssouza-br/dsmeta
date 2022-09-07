796214984f36a05ed289b5d4e17ef8f1d8235567
Sergio Souza <sergio_unicamp@yahoo.com.br>
2022-09-07 20:49:13 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/Notification button"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return(
     <>
-      <h1>"Olá mundo"</h1>
+      <Header/>
+      <main>
+        <section id="sales">
+            <div className="dsmeta-container">
+              <SalesCard/>
+            </div>
+        </section>
+      </main>
+
       <NotificationButton/>
     </>
   )
